df45d87357a3fbe339f240ed9bc5eac179f84f4f
ZCJ <zcj409861716@outlook.com>
2019-02-19 00:01:59 +0800

    加入elementUI

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,12 +1,43 @@
 <template>
-   <div>登录组建</div>
+   <div>
+     <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="用户名">
+        <el-input
+          v-model="userForm.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input
+          type='password'
+          v-model="userForm.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login">登录</el-button>
+      </el-form-item>
+    </el-form>
+
+   </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
-
+      userForm:{
+        username:'',
+        password:''
+      }
     }
+  },
+  methods: {
+   async login () {
+     const res=await axios.post('http://localhost:8888/api/private/v1/login',this.userForm)
+     const data=res.data
+     if(data.meta.status===200){
+       this.$router.push({
+         name:'home'
+       })
+     }
+   }
   }
 }
 </script>
