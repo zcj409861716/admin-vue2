28bb4834f8fdbda12f0d8d6935e5e11563364383
ZCJ <zcj409861716@outlook.com>
2019-02-18 22:22:09 +0800

    :tada:Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>新项目123</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/login/login.vue
@@ -0,0 +1,14 @@
+<template>
+   <div>登录组建</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style>
+</style>
